01d3879d23d1ad4426188e0b02ec3d3e07a4bebc
Benzzzai <44294294+Benzzzai@users.noreply.github.com>
2018-11-03 16:38:03 +0100

    Add files via upload

---
+++ player.cpp
@@ -0,0 +1,20 @@
+#include "player.h"
+
+Player::Player(int x){
+    this->money = x;
+}
+
+void earnmoney(){
+    //money++;
+}
+
+void buyUnit(int t){
+    /*Unit* uni = new Unit();
+    army.add(uni)
+    money -= cara[c][uni.type];*/
+}
+
+void resetUnit() {
+    /*for (unit in army){
+        unit.played = false;*/
+}
